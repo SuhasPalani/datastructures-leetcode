09702622e5c70ef34c972837279d7587561f7e99
Suhas Palani <sugipals007@gmail.com>
2024-01-04 17:49:19 -0600

    simple ex1

---
+++ Big O/ex1.py
@@ -0,0 +1,32 @@
+# <> Big O: O(n)
+# Implement a Python function called print_items.
+
+# This function should take a single integer as an argument and print out a sequence of numbers from 0 up to, but not including, the provided integer.
+
+# The function should use a for loop and Python's built-in range function to generate the sequence of numbers.
+
+# The function signature should be: def print_items(n):
+
+
+# Example:
+
+# Here is an example of how your function should behave:
+
+
+# If you call print_items(10), your function should print:
+
+
+## WRITE THE PRINT_ITEMS FUNCTION HERE ##
+#                                       #
+#                                       #
+#                                       #
+#                                       #
+#########################################
+def print_items(n):
+    for i in range(n):
+        print(i)
+        
+      
+        
+# DO NOT CHANGE THIS LINE:
+print_items(10)
