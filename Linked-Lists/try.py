4b09cd709f814464e6ea7b6811adc5d72d17cfc7
Suhas Palani <sugipals007@gmail.com>
2024-03-02 12:46:46 -0600

    underthehood

---
+++ Linked-Lists/try.py
@@ -0,0 +1,17 @@
+# 11->3->23->7 
+head={
+    'value':11,
+    'next':{
+        'value':3,
+        'next':{
+        'value':23,
+    'next':{
+        'value':7,
+    'next': None      
+            
+    }
+    }
+    }
+}
+
+print(head['next']['next']['value'])
