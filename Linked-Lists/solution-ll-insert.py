df096eec889988666b0536c4d417bf048fd1cdbd
Suhas Palani <sugipals007@gmail.com>
2024-03-02 15:34:57 -0600

    insert solution

--- Linked-Lists/solution-ll-insert.py
+++ Linked-Lists/solution-ll-insert.py
@@ -81,19 +81,19 @@ class LinkedList:
             return True
         return False
     
-    def insert(self, index,value):
-        if index <0 or index>=self.length:
+    def insert(self, index, value):
+        if index < 0 or index > self.length:
             return False
-        if index==0:
+        if index == 0:
             return self.prepend(value)
-        if index==self.length:
+        if index == self.length:
             return self.append(value)
-        new_node=Node(value)
-        temp=self.get(index-1)
-        new_node.next=temp.next
-        temp.next=new_node
-        self.length+=1
-        return True
+        new_node = Node(value)
+        temp = self.get(index - 1)
+        new_node.next = temp.next
+        temp.next = new_node
+        self.length += 1   
+        return True  
   
 
 
