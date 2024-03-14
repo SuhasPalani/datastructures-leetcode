243c8b54c2ae4b1b90b2b20c64e7fae765ced380
Suhas Palani <sugipals007@gmail.com>
2024-03-13 19:53:25 -0500

    solution enqueue

--- Stacks-Queues/EXERCISE-Queue-Enqueue.py
+++ Stacks-Queues/EXERCISE-Queue-Enqueue.py
@@ -17,12 +17,16 @@ class Queue:
             print(temp.value)
             temp = temp.next
         
-    ## WRITE ENQUEUE METHOD HERE ##
-    #                             #
-    #                             #
-    #                             #
-    #                             #
-    ###############################
+    def enqueue(self,value):
+        new_node=Node(value)
+        if self.first is None:
+            self.first=new_node
+            self.last=new_node
+        
+        else:
+            self.last.next=new_node
+            self.last=new_node
+        self.length+=1
         
 
 
