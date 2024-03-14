b4d225d440935b6897eda59abf48883c074245fc
Suhas Palani <sugipals007@gmail.com>
2024-03-13 19:58:22 -0500

    solution dequeue

--- Stacks-Queues/EXERCISE-Queue-Dequeue.py
+++ Stacks-Queues/EXERCISE-Queue-Dequeue.py
@@ -33,8 +33,8 @@ class Queue:
             return None
         temp=self.first
         if self.length==1:
-            self.first==None
-            self.last==None
+            self.first=None
+            self.last=None
         else:
             self.first=self.first.next
             temp.next=None
