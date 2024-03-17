04ceb837fc33af2ec52806c80ceb3e482e284e76
Suhas Palani <sugipals007@gmail.com>
2024-03-17 12:50:15 -0500

    insert solution

--- RBST/EXERCISE-BST-R_Insert.py
+++ RBST/EXERCISE-BST-R_Insert.py
@@ -42,7 +42,7 @@ class BinarySearchTree:
 
 
     def __r_contains(self, current_node, value):
-        if current_node == None: 
+        if current_node is None: 
             return False      
         if value == current_node.value:
             return True 
@@ -55,12 +55,19 @@ class BinarySearchTree:
         return self.__r_contains(self.root, value)
 
                   
-    ## WRITE R_INSERT METHODS HERE ##
-    #                               #
-    #                               #
-    #                               #
-    #                               #
-    #################################
+    def __r_insert(self, current_node, value):
+        if current_node is None: 
+            return Node(value)   
+        if value < current_node.value:
+            current_node.left = self.__r_insert(current_node.left, value)
+        if value > current_node.value:
+            current_node.right = self.__r_insert(current_node.right, value) 
+        return current_node    
+
+    def r_insert(self, value):
+        if self.root is None: 
+            self.root = Node(value)
+        self.__r_insert(self.root, value)
 
 
 
